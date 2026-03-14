5f680c7f33ab9707d017bca91520e38576fda25f
Saksham Bhore <140269497+engineershree@users.noreply.github.com>
2026-03-14 14:36:11 +0530

    Add files via upload

---
+++ java_5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class java_5 {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int rows = sc.nextInt();
+            int number = 1;
+            
+            for (int i = 1; i <= rows; i++) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(number + " ");
+                    number++;
+                }
+                System.out.println();
+            }
+        }
+    }
+}
